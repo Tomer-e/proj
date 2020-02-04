9b3fb0234b2282c9e7f72f9a2d714aaa1c0b9adf
tomerel <tomer@eliyahu.com>
2020-02-04 23:14:22 +0200

    run on checkpoints, pcc and pensieve

--- PCC-v/correct_queries/marabou_run_on_checkpoints.py
+++ PCC-v/correct_queries/marabou_run_on_checkpoints.py
@@ -3,7 +3,7 @@ import numpy as np
 from tensorflow.python.saved_model import tag_constants
 
 
-from marabou_pcc_tf import *
+from marabou_K_query1 import *
 
 
 
@@ -12,28 +12,27 @@ import sys
 import os
 
 def main():
-    if len(sys.argv) not in [2, 3]:
-        print("usage:", sys.argv[0], "<pb_filename_prefix> [-l]")
+    if len(sys.argv) not in [3]:
+        print("usage:", sys.argv[0], "<pb_filename_prefix> [k]")
         exit(0)
 
     pb_filename_format = '{}_{}.{}'
     #.format(checkpoints_dir,model_name,idx,suffix) # checkpoints_dir+"/"+model_name++str(0)+
     idx = 0
     pb_filename_prefix = sys.argv[1]
+    k = int(sys.argv[2])
     # print(pb_filename_format.format(pb_filename_prefix,idx, "pb"))
+    results = []
     while os.path.isfile(pb_filename_format.format(pb_filename_prefix,idx, "pb")):
-        if idx == 2:
-            idx+=1
         pb_filename = pb_filename_format.format(pb_filename_prefix,idx, "pb")
         print("-------------------------------------------------")
         print("                    checkpoint_"+str(idx))
         print("-------------------------------------------------")
         print(pb_filename)
-        print("=========================-basic_test-=========================")
-        basic_test(pb_filename, len(sys.argv) == 3)
-        print("=========================-basic_test_2-=========================")
-        basic_test_2(pb_filename, len(sys.argv) == 3)
+        results.append(k_test(pb_filename, k, False))
+        # basic_test(pb_filename, len(sys.argv) == 3)
         idx+=1
+    print(results)
 
 if __name__ == "__main__":
     main()
